00e431173400410a584af38f54a9b38b060ce99a
atix1906 <27219487+atix1906@users.noreply.github.com>
2018-03-13 14:17:11 +0100

    Add files via upload
    fix

--- inc.sh
+++ inc.sh
@@ -5,6 +5,6 @@ do
 	echo $inc
     inc=$((inc+1))
 	
-    
+    echo -e "import redis\nr = redis.StrictRedis(host=\"$REDIS_HOST\", port=$REDIS_PORT, db=0)\nr.rpush(\"rng::list\", $inc)" | python3
 	sleep 1
 done
